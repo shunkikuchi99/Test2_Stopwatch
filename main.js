8167cfbeb249c8f345f764497875b61e0c51de9d
shunkikuchi99 <shunkikuchi99@gmail.com>
2022-11-12 13:41:17 +0000

    課題２＿電卓です

---
+++ main.js
@@ -0,0 +1,16 @@
+function clickbutton(target){
+  let result=document.getElementById("result")
+  let target_value=target.innerHTML;
+  
+  if (target_value=="AC"){
+      result.innerHTML="0"
+  } else if (target_value=="="){
+      result.innerHTML=eval(result.innerHTML)
+  } else {
+      if(result.innerHTML=="0"){
+        result.innerHTML=target_value
+      }else{
+        result.innerHTML+=target_value
+      }
+  }
+ }
