6023dc09ced3d274688af1e3eb625f8d583b111f
shunkikuchi99 <shunkikuchi99@gmail.com>
2022-11-17 07:40:54 +0000

    課題２電卓＿変更

--- main.js
+++ main.js
@@ -13,4 +13,27 @@ function clickbutton(target){
         result.innerHTML+=target_value
       }
   }
- }
+}
+
+$(document).ready(function(){
+  $(".button").click(function(){
+    let get_text=$("display").text();
+    let target_value = document.getElementsByClassName("button")
+
+    var str = get_text;
+    var slicsSymbol_1 = str.slice(-1); //右から１文字目を指定
+    var slicsSymbol_2 = str.slice(-1,-2); //右から２文字目を指定
+  
+    var sourceStr = get_text;
+    var cutSymbol = sourceStr.slice(-1,-2); //右から２文字目を指定
+  
+    if(slicsSymbol_1 && slicsSymbol_2=="/"||"*"||"-"||"+"){ //右１文字目と二文字目を取得
+      slicsSymbol_2 = cutSymbol
+    }else {
+      if(get_text.slicsSymbol_1==target_value){
+        get_text+=target_value
+      }
+    }
+  });
+  
+});
